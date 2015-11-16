6e97da32a7aa219789aecae75962d16c496a7e02
steph-harris <steph.harris21@gmail.com>
2015-11-16 00:50:38 -0500

    added js scripts folder, changed btn innerHTML on click in js

---
+++ scripts/practice.js
@@ -0,0 +1,9 @@
+var btnMove = document.getElementById("changeH");
+var htext = document.getElementsByTagName("h1");
+btnMove.addEventListener("click", changeTo);
+
+debugger;
+function changeTo (){
+// htext[0].setAttribute("class", "color:red;");
+btnMove.innerHTML = "changed?";
+}
