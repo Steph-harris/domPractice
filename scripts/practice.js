cda2e01e054e883401cbabf6ed428e0d53c53d30
steph-harris <steph.harris21@gmail.com>
2015-12-04 17:27:50 -0500

    added modal with basic picture swap on mouseover

--- scripts/practice.js
+++ scripts/practice.js
@@ -18,6 +18,10 @@ $(document).ready(function() {
   $(".btn-primary").on("mouseover", function(){
     $(".alert-info").fadeToggle(2000);
   });
+
+  $("#changePic").on("mouseover", function(){
+    $("#changePic").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJCjY_mGthMIKXlo4C-f51IUnhEJ3ciTEj4dNnqAv4rFkzgnAIkw")
+  });
 });
 
 
